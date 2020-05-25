a5a926f4a10d2bd9f6010988717e36862b14a9b8
ahmtyldr <65905954+ahmtyldr@users.noreply.github.com>
2020-05-26 01:47:02 +0300

    Add files via upload

---
+++ ödev8/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+  
+    public static void main(String[] args) {
+        örnek test = new örnek();
+        
+        test.setŞifre(123);//burda diğer calassdaki private olan
+    }                      //erişim belirleyiciye 
+}
---
+++ ödev8/src/örnek.java
@@ -0,0 +1,20 @@
+ public class örnek {
+
+    private int şifre = 123; // burda private olan bir erişim belirleyiciye
+    //değer atadık
+
+    public int getŞifre() {
+        return şifre;          //burda setter ve getter methodlarını
+    }                          //oluşturduk
+
+    public void setŞifre(int şifre) {  // burda set methodunda bir koşul oluşturduk
+
+        if (şifre == 123) {      // eğer karşıda girilen değer burdaki değerle uyuşmazsa 
+            System.out.println("şifre doğru");//erişim belirleyicinin içine 
+        } else {                              //giremeyeceğiz
+            System.out.println("şifre yanlış");
+        }
+        this.şifre = şifre;
+    }
+
+}
